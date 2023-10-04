1829289fc98b9e576eb61470e5f780fb91a82e5e
sauceboss44 <alexrudakov5@gmail.com>
2023-10-04 22:15:54 +0100

    change integerfield constraints again

--- things/models.py
+++ things/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Thing(models.Model):
     name = models.CharField(unique=True, blank=False, max_length=30)
-    description = models.TextField(unique=False, blank=True, max_length=120)
-    quantity = models.PositiveIntegerField(default=1, validators=[
+    description = models.CharField(unique=False, blank=True, max_length=120)
+    quantity = models.PositiveIntegerField(default=1,
+        validators=[
             MaxValueValidator(100),
-            MinValueValidator(1)
+            MinValueValidator(2)
         ])
